90825b04a4c33491beb9d06a1ecc23df5d8f6661
zelo0 <minorSunbi@gmail.com>
2022-11-21 20:57:05 +0900

    indent 2를 넘지 않게 리팩토링

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -17,14 +17,13 @@ public class Game {
     public void start() {
         while (true) {
             answer = pickThreeNumbers();
+            System.out.println("answer = " + answer);
             proceed();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String input = scanner.nextLine();
-            if (input.equals("1")) {
-                continue;
-            } else if (input.equals("2")) {
+            if (input.equals("2")) {
                 break;
-            } else {
+            } else if (!input.equals("1")) {
                 throw new IllegalArgumentException();
             }
         }
@@ -43,47 +42,62 @@ public class Game {
     }
 
     private String verifyAnswer(String inputNumber) {
+        int strike = 0;
+        int ball = 0;
+
         if (inputNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
 
-        int strike = 0;
-        int ball = 0;
-
         // 자리수 일치 여부 확인
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (inputNumber.charAt(i) == answer.charAt(j)) {
-                    if (i == j) {
-                        strike++;
-                    } else {
-                        ball++;
-                    }
-                    break;
-                }
+            if (isStrike(inputNumber.charAt(i), answer.charAt(i))) {
+                strike++;
+            } else if (isBall(i, inputNumber, answer)) {
+                ball++;
             }
         }
 
+        return getJudgeString(strike, ball);
+    }
+
+    private String getJudgeString(int strike, int ball) {
         if (strike == 0 && ball == 0) {
             return "낫싱";
         }
-        StringBuffer ret = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         if (ball > 0) {
-            ret.append(ball);
-            ret.append("볼");
+            sb.append(ball);
+            sb.append("볼");
             if (strike > 0) {
-                ret.append(" ");
+                sb.append(" ");
             }
         }
         if (strike > 0) {
-            ret.append(strike);
-            ret.append("스트라이크");
+            sb.append(strike);
+            sb.append("스트라이크");
         }
-        return ret.toString();
+        return sb.toString();
+    }
+
+    private boolean isBall(int inputIndex, String inputNumber, String answer) {
+        for (int i = 0; i < 3; i++) {
+            if (i == inputIndex) {
+                continue;
+            }
+            if (inputNumber.charAt(inputIndex) == answer.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isStrike(char input, char answer) {
+        return input == answer;
     }
 
     private String pickThreeNumbers() {
-        ArrayList<Integer> answer = new ArrayList<Integer>(3);
+        ArrayList<Integer> answer = new ArrayList<>(3);
         while (answer.size() < 3) {
             int number = RandomUtils.nextInt(1, 9);
             if (!answer.contains(number)) {
