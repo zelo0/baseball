f6b89dc23d49a34a27ff8c7e78d82180686ce5e3
zelo0 <minorSunbi@gmail.com>
2022-11-24 15:16:15 +0900

    refactor: 유저의 입력을 받고 검증하는 클래스 User 추가 refactor: while을 do while로 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,49 +4,30 @@ package baseball;
 import java.util.Scanner;
 
 public class Game {
-    private final Scanner scanner;
     private Computer computer;
+    private User user;
 
     public Game(Scanner scanner) {
-        this.scanner = scanner;
+        computer = new Computer();
+        user = new User(scanner);
     }
 
     public void start() {
         computer = new Computer();
-        while (true) {
+        do {
             computer.pickThreeNumbers();
-            proceed();
-            askIfEnd();
-        }
+            guess();
+        } while (!user.askIfEnd());
     }
 
-    private void askIfEnd() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = scanner.nextLine();
-        if (input.equals("2")) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void proceed() {
-        Validator validator = new Validator();
-
+    private void guess() {
         while (true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String inputNumber = scanner.nextLine();
-            validate(validator, inputNumber);
+            String inputNumber = user.takeInputNumber();
             String output = computer.verifyAnswer(inputNumber);
             System.out.println(output);
             if (output.equals("3스트라이크")) {
                 break;
             }
-        }
-    }
-
-    private void validate(Validator validator, String inputNumber) {
-        if (!validator.isNumber(inputNumber) || !validator.isLengthThree(inputNumber)
-            || !validator.isRightRangeNumber(inputNumber) || !validator.isAllDifferentNumber(inputNumber)) {
-            throw new IllegalArgumentException();
         }
     }
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import java.util.Scanner;
+
+/* 유저로부터 입력받는 클래스 */
+public class User {
+    private final Scanner scanner;
+    private static final Validator validator = new Validator();
+
+    public User(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String takeInputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = scanner.nextLine();
+        validate(validator, inputNumber);
+        return inputNumber;
+    }
+
+    public boolean askIfEnd() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = scanner.nextLine();
+        if (isEnd(input)) {
+            throw new IllegalArgumentException();
+        }
+        return false;
+    }
+
+    private boolean isEnd(String input) {
+        return input.equals("2");
+    }
+
+    private void validate(Validator validator, String inputNumber) {
+        if (!validator.isNumber(inputNumber) || !validator.isLengthThree(inputNumber)
+                || !validator.isRightRangeNumber(inputNumber) || !validator.isAllDifferentNumber(inputNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
